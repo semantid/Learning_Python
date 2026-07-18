c328484573fd476357064a56ccf924c08bcf8136
Semanti Dutta <semanti.dutta2010@gmail.com>
2026-07-18 21:43:24 +0530

    Allow repeated conversions at User's choice

--- hello.py
+++ hello.py
@@ -1,21 +1,29 @@
-def Celsius_to_Fahrenheit (c):
+def Celsius_to_Fahrenheit(c):
     return (c * 9/5) + 32
 
-def Fehrenheit_to_Celsius (f):
+def Fahrenheit_to_Celsius(f):
     return (f - 32) * 5/9
 
 def main():
     print (" Temperature converter")
-    type=input("Temperature type Celsius or Fahrenheight (C or F)").strip().upper()
+    
 
-    if type== "C":
+while True:
+
+    choice=input("Temperature type Celsius or Fahrenheight (C or F): ").strip().upper()
+
+    if choice=="QUIT":
+        print("Good Bye!")
+        break 
+             
+    elif choice== "C":
         temp=float(input("Enter temperature in Celcius: "))
         result=Celsius_to_Fahrenheit(temp)
         print(f"{temp} C is {result:.2f} F")
 
-    elif type=="F":
+    elif choice=="F":
          temp=float(input("Enter temperature in Fahrenheight:  "))
-         result=Fehrenheit_to_Celsius(temp)
+         result=Fahrenheit_to_Celsius(temp)
          print(f"{temp} F is {result:.2f} C")         
       
     else:
